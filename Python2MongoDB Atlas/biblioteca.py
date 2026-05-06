370cb10efbdba5dcb0a804701e5580bff1dfb44b
mleiva <mleiva.robles@gmail.com>
2026-05-06 17:23:39 -0400

    Se genera listado con todos los prestamos

--- Python2MongoDB Atlas/biblioteca.py
+++ Python2MongoDB Atlas/biblioteca.py
@@ -8,11 +8,13 @@ cliente = MongoClient(os.getenv('MONGO_URI'))
 db = cliente['biblioteca']
 
 #todos los socios
+print()
 for s in db.socios.find():
     #print (s)
     print(f"[{s['_id']}] {s['nombre']} — {s['email']} - {s['telefono']} - {s['fecha_inscripcion']}")
 
 #todos los libros y autores
+print()
 for l in db.libros.find():
     #print(l)
     print(f"libro: {l['titulo']} - {l['genero']} - {l['anio_publicacion']} y stock= {l['stock']}")
@@ -20,6 +22,7 @@ for l in db.libros.find():
         print(f"  autores: {a['nombre']}")
 
 #todos los prestamos
+print()
 for p in db.prestamos.find().sort("_id",1):
     #print(p)
     titulo  = p.get("libro").get("titulo") 
@@ -27,14 +30,13 @@ for p in db.prestamos.find().sort("_id",1):
     socio = db.socios.find_one({"_id": nro_socio})
     
     n_multas = len(p.get("multas"))
-    s_multas = f"El libro {titulo} fue prestado a {socio['nombre']}"
+    s_prestamo = f"El libro '{titulo}' fue prestado a {socio['nombre']}"
 
-    if (n_multas == 0):
-        print(" y no tiene multas")
+    if n_multas == 0:
+        print(f"{s_prestamo} y no tiene multas")
     else:
-        for i in range(1,n_multas):
-            lst_multa = p.get("multas").find_one({"id_multa": i})
-            monto = lst_multa.get("monto")
-            motivo = lst_multa.get("multas")
-            print(f" y tiene un multa de {monto}y por {motivo}")
+        lst_multa = p.get("multas")[0] #imprime el primer elemento
+        monto = lst_multa.get("monto")
+        motivo = lst_multa.get("motivo")
+        print(f"{s_prestamo} y tiene un multa de {monto} y por {motivo}")
         
